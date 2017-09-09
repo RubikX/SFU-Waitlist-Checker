f40955013deb36e68314739c132831189b125ee1
rubikX <ohtehinternets@hotmail.com>
2017-09-08 22:27:32 -0700

    Author name

--- waitlist.py
+++ waitlist.py
@@ -1,3 +1,5 @@
+# Author: Edison Suen
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
--- waitlist_test.py
+++ waitlist_test.py
@@ -1,3 +1,4 @@
+# Author: Edison Suen
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
